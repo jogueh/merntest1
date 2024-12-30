f4fed5b54b097ecea4dcfa45d760586b450c807a
Jada Ogueh <jadaogueh@jmb.attlocal.net>
2024-12-30 14:13:09 -0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express'); 
+
+const mongoose = require('mongoose'); 
+
+const cors = require('cors'); 
+
+require('dotenv').config(); 
+const app = express(); 
+
+app.use(cors()); 
+
+app.use(express.json()); 
+
+const dbURI = process.env.MONGO_URI; // MongoDB URI from Azure Cosmos DB 
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("MongoDB Connected")).catch((err) => console.log("MongoDB Connection Error:", err));
+
+const itemRoutes = require('./routes/itemRoutes'); 
+
+app.use('/api/items', itemRoutes); 
+
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, () => { 
+
+  console.log(`Server running on port ${PORT}`); 
+
+}); 
